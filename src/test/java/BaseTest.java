ab2d38ec4a0bd125b997ab9feb3c114c5b6e060b
Aleksandar Radovanović <radovanovicsasha.rs@gmail.com>
2026-04-11 11:22:59 +0200

    Enable headless Chrome only in CI.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,27 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeEach
-    public void setUp() {
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
 
-        driver = new ChromeDriver();
+        // Headless samo na GitHub Actions
+        if (System.getenv("GITHUB_ACTIONS") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.militaryshop.rs/");
-        System.out.println("Entered succesfully on testing web-site");
+        System.out.println("Entered successfully on testing web-site");
     }
 
     @AfterEach
@@ -22,3 +31,27 @@ public class BaseTest {
         }
     }
 }
+//import org.junit.jupiter.api.*;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class BaseTest {
+//
+//    protected WebDriver driver;
+//
+//    @BeforeEach
+//    public void setUp() {
+//
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://www.militaryshop.rs/");
+//        System.out.println("Entered succesfully on testing web-site");
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//}
