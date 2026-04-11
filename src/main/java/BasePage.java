c2bdbe9584bbdfa3eab1f32408b3a221b2322f10
Aleksandar Radovanović <radovanovicsasha.rs@gmail.com>
2026-04-11 11:12:14 +0200

    Move page classes to src/main for Maven CI compatibility.

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -1,9 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import java.time.Instant;
 
 public class BasePage {
 
