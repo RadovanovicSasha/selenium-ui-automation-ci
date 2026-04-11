602db7ecf0b6b2e876026d935fa8305fc2d419ca
Aleksandar Radovanović <radovanovicsasha.rs@gmail.com>
2026-04-11 11:31:28 +0200

    Stabilize login flow for CI.

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -3,26 +3,56 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends BasePage {
 
-
     // LOKATORI:
-    private final By myAccountLink = By.cssSelector("a[href*='moj-nalog']");
     private final By usernameInput = By.id("username");
     private final By passwordInput = By.id("password");
-    private final By loginButton   = By.name("login");
+    private final By loginButton = By.name("login");
 
-    // PROSLEĐUJEMO DRIVER (ka BasePage) - POM KONSTRUKTOR
+    // POM konstruktor
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
     // BIZNIS METODA:
-
     public void logovanje() {
-        driver.findElement(myAccountLink).click();
+
+        // direktan odlazak na login stranu (stabilnije za CI)
+        driver.get("https://www.militaryshop.rs/moj-nalog/");
+
         driver.findElement(usernameInput).clear();
         driver.findElement(usernameInput).sendKeys("radovanovicsasha@yahoo.com");
+
         driver.findElement(passwordInput).clear();
         driver.findElement(passwordInput).sendKeys("skolskimilitari");
+
         driver.findElement(loginButton).click();
     }
 }
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//
+//public class LoginPage extends BasePage {
+//
+//
+//    // LOKATORI:
+//    private final By myAccountLink = By.cssSelector("a[href*='moj-nalog']");
+//    private final By usernameInput = By.id("username");
+//    private final By passwordInput = By.id("password");
+//    private final By loginButton   = By.name("login");
+//
+//    // PROSLEĐUJEMO DRIVER (ka BasePage) - POM KONSTRUKTOR
+//    public LoginPage(WebDriver driver) {
+//        super(driver);
+//    }
+//
+//    // BIZNIS METODA:
+//
+//    public void logovanje() {
+//        driver.findElement(myAccountLink).click();
+//        driver.findElement(usernameInput).clear();
+//        driver.findElement(usernameInput).sendKeys("radovanovicsasha@yahoo.com");
+//        driver.findElement(passwordInput).clear();
+//        driver.findElement(passwordInput).sendKeys("skolskimilitari");
+//        driver.findElement(loginButton).click();
+//    }
+//}
